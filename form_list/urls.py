8feacd6db86f086da83fdb34dd142c80381fe7df
jiwoo-hongb <jwhong48@gmail.com>
2025-06-07 05:57:27 +0900

    <상세내역 더 보기>기능버튼 활성화

--- form_list/urls.py
+++ form_list/urls.py
@@ -1,10 +1,12 @@
 # form_list/urls.py
+
 from django.urls import path
 from . import views
-from .views import my_applications_view
+from .views import my_applications_view, application_detail_view
 
 app_name = 'form_list'
 
 urlpatterns = [
-    path('list/', my_applications_view, name='application_list'),
+    path('detail/<int:application_id>/', views.application_detail_view, name='application_detail'),
+    path('my-applications/', views.my_applications_view, name='my_applications'),
 ]
--- form_list/views.py
+++ form_list/views.py
@@ -1,12 +1,23 @@
 # form_list/views.py
-from django.shortcuts import render, redirect
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from .models import UserApplication
+from form.models import Application
+
+@login_required(login_url='/sign/login/')
+def application_detail_view(request, application_id):
+    user_application = get_object_or_404(UserApplication, id=application_id, user=request.user)
+    application = get_object_or_404(Application, user=request.user, club=user_application.club)
+
+    return render(request, 'form_list/application_detail.html', {
+        'application': application,
+    })
+
+# form_list/views.py
 
 @login_required(login_url='/sign/login/')
 def my_applications_view(request):
-    applications = UserApplication.objects.filter(user=request.user).order_by('submitted_at')
-
+    applications = UserApplication.objects.filter(user=request.user).order_by('-submitted_at')
     return render(request, 'form_list/application_list.html', {
         'applications': applications,
     })
